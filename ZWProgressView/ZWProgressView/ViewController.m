41f7284e336082658f7c63741be81f2e408118af
Victor Ilisei <vilisei@Victors-MacBook-Pro.local>
2014-06-17 21:54:05 -0700

    Added custom init method for progress bar fill color

--- ZWProgressView/ZWProgressView/ViewController.m
+++ ZWProgressView/ZWProgressView/ViewController.m
@@ -19,9 +19,8 @@
     
     self.view.backgroundColor = [UIColor whiteColor];
     
-    ZWProgressView *progressView = [[ZWProgressView alloc] initWithFrame:CGRectMake((self.view.bounds.size.width - 200) / 2.0f, (self.view.bounds.size.height - 50) / 2.0f, 200, 50)];
+    ZWProgressView *progressView = [[ZWProgressView alloc] initWithFrame:CGRectMake((self.view.bounds.size.width - 200) / 2.0f, (self.view.bounds.size.height - 50) / 2.0f, 200, 50) progressColor:[UIColor colorWithRed:0.0f green:0.0f blue:0.5f alpha:1.0f]];
     progressView.progress = 0.47f;
-    progressView.progressBar.backgroundColor = [UIColor colorWithRed:0.0f green:0.0f blue:0.5f alpha:1.0f];
     
     [self.view addSubview:progressView];
 }
